45fd415e79d0e29917888e0ccc9e4da8c26c4f5e
Yashaswini7274 <yashaswini7274@gmail.com>
2026-01-29 23:43:59 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,6 +1,6 @@
 public class Area { 
     public static void main(String[] args) {
-        double side = 5.0;
+        double side = 8.0;
         double area = side * side ;
         System.out.println("this is the area of the square:" + area);
     }
