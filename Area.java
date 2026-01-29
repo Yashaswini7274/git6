d8d8abea79d583e22e272e4952ecab4fb1c285d9
Yashaswini7274 <yashaswini7274@gmail.com>
2026-01-29 23:41:45 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,7 @@
+public class Area { 
+    public static void main(String[] args) {
+        double side = 5.0;
+        double area = side * side ;
+        System.out.println("this is the area of the square:" + area);
+    }
+}
